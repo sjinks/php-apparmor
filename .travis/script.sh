df7a1dea9c33b14136c01af70c4ae265927eb052
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2016-02-22 23:15:21 +0200

    Debug

--- .travis/script.sh
+++ .travis/script.sh
@@ -14,4 +14,4 @@ make install
 php -i > /dev/null
 php -m > /dev/null
 
-(cd $DIR; php "$DIR/index.php" > "$DIR/out.001"; cat "$DIR/out.001"; cmp "$DIR/out.001" "$DIR/expected.001")
+(cd $DIR; php index.php > "$DIR/out.001"; cat "$DIR/out.001"; cmp "$DIR/out.001" "$DIR/expected.001")
