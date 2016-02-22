f35d93a72b340b3d137a95848a5149a1f0b69d9b
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2016-02-22 19:58:56 +0200

    Travis

---
+++ .travis/script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+DIR=$(readlink -enq $(dirname $0))
+
+phpize
+./configure --silent
+make --silent CFLAGS+="-Wall -Wextra -Wno-unused-parameter -DDEBUG_PHP_AA"
+
+phpenv config-add "$DIR/apparmor.ini"
+
+php -i > /dev/null
+php -m > /dev/null
+
+(cd $DIR && php index.php > out.001 && cmp out.001 expected.001)
