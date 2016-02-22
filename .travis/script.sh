8b352b7945c502c1bc4ec2910ea41877d99664e7
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2016-02-22 21:28:41 +0200

    Bug fixes

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-set -e
-set -x
+#set -e
 
 DIR=$(readlink -enq $(dirname $0))
 
@@ -15,6 +14,6 @@ make install
 php -i > /dev/null
 php -m > /dev/null
 
-php index.php > "$DIR/out.001"
+php "$DIR/index.php" > "$DIR/out.001"
 ls -lha "$DIR/"
 cmp "$DIR/out.001" "$DIR/expected.001"
